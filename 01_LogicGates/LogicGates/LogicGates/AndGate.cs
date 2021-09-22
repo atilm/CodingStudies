331c2c678a85874a13e9ab602c8c460347054b77
Thielmann, Andreas <andreas.thielmann@hte-company.de>
2021-09-22 21:46:58 +0200

    and gate

---
+++ 01_LogicGates/LogicGates/LogicGates/AndGate.cs
@@ -0,0 +1,25 @@
+﻿using LogicGates.BasicComponents;
+
+namespace LogicGates.LogicGates
+{
+    public class AndGate
+    {
+        public AndGate()
+        {
+            Voltage.Output.Connect(Relay1.VoltageInput);
+            Input1 = Relay1.Input;
+            Relay1.Output.Connect(Relay2.VoltageInput);
+            
+            Input2 = Relay2.Input;
+            Output = Relay2.Output;
+        }
+
+        public Input Input1 { get; }
+        public Input Input2 { get; }
+        public Output Output { get; }
+
+        private VoltageSource Voltage { get; } = new();
+        private Relay Relay1 { get; } = new();
+        private Relay Relay2 { get; } = new();
+    }
+}
---
+++ 01_LogicGates/LogicGatesTests/LogicGates/AndGateTests.cs
@@ -0,0 +1,41 @@
+﻿using LogicGates.BasicComponents;
+using LogicGates.LogicGates;
+using NUnit.Framework;
+
+namespace LogicGatesTests.LogicGates
+{
+    [TestFixture]
+    public class AndGateTests
+    {
+        [Test]
+        public void AndGateTest()
+        {
+            var voltage1 = new VoltageSource();
+            var voltage2 = new VoltageSource();
+            var lightBulb = new LightBulb();
+            var orGate = new AndGate();
+            
+            voltage1.SwitchOff();
+            voltage2.SwitchOff();
+
+            voltage1.Output.Connect(orGate.Input1);
+            voltage2.Output.Connect(orGate.Input2);
+            orGate.Output.Connect(lightBulb.Input);
+            
+            lightBulb.ShouldBeOff();
+
+            voltage1.SwitchOn();
+            lightBulb.ShouldBeOff();
+            voltage1.SwitchOff();
+            
+            voltage2.SwitchOn();
+            lightBulb.ShouldBeOff();
+            
+            voltage1.SwitchOn();
+            lightBulb.ShouldBeOn();
+            
+            voltage2.SwitchOff();
+            lightBulb.ShouldBeOff();
+        }
+    }
+}

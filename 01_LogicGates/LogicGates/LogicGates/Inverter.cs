d4ba54d114c1c999cfbb7dfaba882ea8568e92f1
Thielmann, Andreas <andreas.thielmann@hte-company.de>
2021-09-25 13:34:12 +0200

    implement inverter

---
+++ 01_LogicGates/LogicGates/LogicGates/Inverter.cs
@@ -0,0 +1,21 @@
+﻿using LogicGates.BasicComponents;
+
+namespace LogicGates.LogicGates
+{
+    public class Inverter
+    {
+        public Inverter()
+        {
+            _source.Output.Connect(_relay.VoltageInput);
+
+            Input = _relay.Input;
+            Output = _relay.InvertedOutput;
+        }
+        
+        public Input Input { get; }
+        public Output Output { get; }
+
+        private readonly Relay _relay = new();
+        private readonly VoltageSource _source = new();
+    }
+}
---
+++ 01_LogicGates/LogicGatesTests/LogicGates/InverterTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+using LogicGates.BasicComponents;
+using LogicGates.LogicGates;
+using NUnit.Framework;
+
+namespace LogicGatesTests.LogicGates
+{
+    [TestFixture]
+    public class InverterTests
+    {
+        [Test]
+        [TestCase(Voltage.Off, LightBulb.BulbState.On)]
+        [TestCase(Voltage.On, LightBulb.BulbState.Off)]
+        public void InverterTest(Voltage inputVoltage, LightBulb.BulbState expectedState)
+        {
+            var source = new VoltageSource(inputVoltage);
+            var inverter = new Inverter();
+            var bulb = new LightBulb();
+
+            source.Output.Connect(inverter.Input);
+            inverter.Output.Connect(bulb.Input);
+
+            bulb.State.Should().Be(expectedState);
+        }
+    }
+}

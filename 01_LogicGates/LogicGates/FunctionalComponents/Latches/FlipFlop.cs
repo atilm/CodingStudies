99b4622e7cbf4dcf97070dc497c1a0e6cf807870
Thielmann, Andreas <andreas.thielmann@hte-company.de>
2021-10-01 21:42:13 +0200

    flipflop

---
+++ 01_LogicGates/LogicGates/FunctionalComponents/Latches/FlipFlop.cs
@@ -0,0 +1,30 @@
+﻿using LogicGates.BasicComponents;
+using LogicGates.LogicGates;
+
+namespace LogicGates.FunctionalComponents.Latches
+{
+    public class FlipFlop
+    {
+        public FlipFlop()
+        {
+            InputA = _norA.Input1;
+            InputB = _norB.Input1;
+
+            _norB.Output.Connect(_norA.Input2);
+            _norA.Output.Connect(_norB.Input2);
+            
+            Q = _norA.Output;
+            QBar = _norB.Output;
+        }
+        
+        public Input InputA { get; }
+        public Input InputB { get; }
+        public Output Q { get; }
+        public Output QBar { get; }
+
+        private NOrGate _norA { get; } = new();
+        private NOrGate _norB { get; } = new();
+        private OutputWireHub _hubA { get; } = new();
+        private OutputWireHub _hubB { get; } = new();
+    }
+}
---
+++ 01_LogicGates/LogicGatesTests/FunctionalComponents/LatchesTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using LogicGates.BasicComponents;
+using LogicGates.FunctionalComponents.Latches;
+using NUnit.Framework;
+
+namespace LogicGatesTests.FunctionalComponents
+{
+    [TestFixture]
+    public class LatchesTests
+    {
+        [Test]
+        public void FlipFlopTest()
+        {
+            var flipFlop = new FlipFlop();
+
+            flipFlop.Q.Voltage.Should().Be(Voltage.Off);
+            flipFlop.QBar.Voltage.Should().Be(Voltage.On);
+
+            flipFlop.InputB.Set(Voltage.On);
+            flipFlop.InputB.Set(Voltage.Off);
+            
+            flipFlop.Q.Voltage.Should().Be(Voltage.On);
+            flipFlop.QBar.Voltage.Should().Be(Voltage.Off);
+            
+            flipFlop.InputA.Set(Voltage.On);
+            flipFlop.InputA.Set(Voltage.Off);
+            
+            flipFlop.Q.Voltage.Should().Be(Voltage.Off);
+            flipFlop.QBar.Voltage.Should().Be(Voltage.On);
+        }
+    }
+}

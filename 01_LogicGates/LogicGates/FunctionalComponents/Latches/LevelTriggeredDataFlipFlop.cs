13b8deb288c3d3993f74c88d8f46374392eb2003
Thielmann, Andreas <andreas.thielmann@hte-company.de>
2021-10-02 14:42:40 +0200

    LevelTriggeredDataFlipFlop

---
+++ 01_LogicGates/LogicGates/FunctionalComponents/Latches/LevelTriggeredDataFlipFlop.cs
@@ -0,0 +1,29 @@
+﻿using LogicGates.BasicComponents;
+using LogicGates.LogicGates;
+
+namespace LogicGates.FunctionalComponents.Latches
+{
+    public class LevelTriggeredDataFlipFlop : IFlipFlop
+    {
+        public LevelTriggeredDataFlipFlop()
+        {
+            Q = _flipFlop.Q;
+            QBar = _flipFlop.QBar;
+            Clock = _flipFlop.Clock;
+            
+            _hub.ConnectSourceInput(Data);
+            _hub.ConnectTargetInput(_inverter.Input);
+            _inverter.Output.Connect(_flipFlop.InputA);
+            _hub.ConnectTargetInput(_flipFlop.InputB);
+        }
+        
+        public Input Clock { get; }
+        public Input Data { get; } = new();
+        public Output Q { get; }
+        public Output QBar { get; }
+
+        private LevelTriggeredFlipFlop _flipFlop = new();
+        private InputWireHub _hub = new();
+        private Inverter _inverter = new();
+    }
+}
